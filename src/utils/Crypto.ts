342561c5582f468431aced9e0c15ea14d2004c17
guta307 <veiga187@gmail.com>
2024-01-29 14:02:33 -0300

    feat: added Authentication function for passwords with hash

--- src/utils/Crypto.ts
+++ src/utils/Crypto.ts
@@ -1,7 +1,15 @@
-import { randomBytes, scryptSync } from 'crypto';
+import { randomBytes, scryptSync, timingSafeEqual } from 'crypto';
 
 export function createHashWithSalt(password: string) {
   const salt = randomBytes(16).toString('hex');
   const passwordHash = scryptSync(password, salt, 64).toString('hex');
   return {salt,passwordHash};
+}
+
+export function Authentication(password,salt,passwordHash){
+       const testeHash = scryptSync(password, salt, 64);
+      const realPassword = Buffer.from(passwordHash, "hex");
+      const matchPassword = timingSafeEqual(testeHash, realPassword);
+
+      return matchPassword
 }
