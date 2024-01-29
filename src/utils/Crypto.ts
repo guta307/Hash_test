20f37d181dacf1103296ae241b69d833bb2f78a1
guta307 <veiga187@gmail.com>
2024-01-29 11:31:43 -0300

    feat: Add utility function for securely hashing passwords with salt

--- src/utils/Crypto.ts
+++ src/utils/Crypto.ts
@@ -1,9 +1,7 @@
 import { randomBytes, scryptSync } from 'crypto';
 
-function createHashWithSalt(password: string) {
+export function createHashWithSalt(password: string) {
   const salt = randomBytes(16).toString('hex');
   const passwordHash = scryptSync(password, salt, 64).toString('hex');
   return {salt,passwordHash};
 }
-
-export default createHashWithSalt
