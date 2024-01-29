5449e860d91a2dc8cbbe5fdde55981d4d8ddb7d6
guta307 <veiga187@gmail.com>
2024-01-29 09:10:58 -0300

    feat: Implement FieldValidation Decorator for Request Validation
    Added the `FieldValidation` decorator function in TypeScript to enhance request validation in our Express application. This decorator dynamically validates the presence of required fields in request bodies, ensuring data integrity and consistency. It checks against a provided template object and returns relevant error messages if required fields are missing. This feature significantly improves the robustness and reliability of our request handling mechanism.

---
+++ src/decorator/FieldValidation.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from 'express';
+
+function FieldValidation<T>(inputObject: T) {
+    return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+      const originalMethod = descriptor.value;
+  
+      descriptor.value = async function(req: Request, res: Response) {
+        
+        try{
+            const inputData = req.body; // Obtenha os dados do corpo da solicitação como T
+  
+        // Obtenha as chaves do objeto exemplo
+        const requiredFields: (keyof T)[] = Object.keys(inputObject) as (keyof T)[];
+  
+        // Verifique se todas as propriedades obrigatórias estão presentes no objeto
+        const missingProperties: string[] = [];
+  
+        for (const key of requiredFields) {
+          if (!(key in inputData)) {
+            missingProperties.push(key as string);
+          }
+        }
+  
+        // Lança um erro se faltarem propriedades
+        if (missingProperties.length > 0) {
+          throw new Error(`Faltam propriedades obrigatórias: ${missingProperties.join(', ')}`);
+        }
+        
+        return originalMethod.apply(this, [req,res]);
+        }
+        catch(e){
+            if (e instanceof Error) {
+                // Se for uma instância de Error, retorne a mensagem de erro
+                return res.status(422).json({ error: e.message });
+              } else {
+                // Para outros tipos de erros, retorne uma mensagem genérica
+                return res.status(500).json({ error: 'Ocorreu um erro desconhecido' });
+              }
+        }
+      }
+    }
+  }
+
+  export default FieldValidation
