93b33aecd90ec91da456a49fc8cdc72166a02ec1
guta307 <veiga187@gmail.com>
2024-01-29 15:38:02 -0300

    feat: Add Email Function for Password Reset
    This commit introduces the `changePasswordEmail` function to the project. This utility function is designed to send email notifications to users requesting a password reset. The email includes a personalized greeting with the user's name and a link for password resetting, incorporating the user's ID and a secure token for authentication. This enhancement improves the user experience by streamlining the password reset process.

--- src/utils/emailSender.ts
+++ src/utils/emailSender.ts
@@ -2,16 +2,16 @@ import nodemailer from 'nodemailer';
 import dotenv from "dotenv";
 dotenv.config();
 
+let transporter = nodemailer.createTransport({
+  service: 'Hotmail',
+auth: {
+  user: process.env.EMAIL,
+  pass: process.env.EMAIL_PASSWORD
+},
+});
+
 export async function validationEmail(email: string, nome: string,id:string,token:string) {
-    // Configuração do transporte de e-mail
-    let transporter = nodemailer.createTransport({
-        service: 'Hotmail',
-      auth: {
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASSWORD
-      },
-    });
-  
+
     // Configuração da mensagem
     await transporter.sendMail({
       from: process.env.EMAIL, // endereço do remetente
@@ -19,6 +19,19 @@ export async function validationEmail(email: string, nome: string,id:string,toke
       subject: "Confirmação de Cadastro", // Assunto
       text: `Olá, ${nome}! Por favor, confirme seu cadastro clicando aqui.`, // corpo da mensagem
       html: `<b>Olá, ${nome}!</b><br>Por favor, confirme seu cadastro clicando <a href="${process.env.ROOT_ADDRESS}/user/validate/${id}?token=${token}">aqui</a>.`, // corpo da mensagem em HTML
+    });
+  
+  }
+
+  export async function changePasswordEmail(email: string, nome: string,id:string,token:string) {
+
+    // Configuração da mensagem
+    await transporter.sendMail({
+      from: process.env.EMAIL, // endereço do remetente
+      to: email, // lista de destinatários
+      subject: "Troca de senha", // Assunto
+      text: `Olá, ${nome}! Clique aqui para recadastrar nova senha.`, // corpo da mensagem
+      html: `<b>Olá, ${nome}!</b><br>Por favor, acesse esse endereço para <a href="${process.env.ROOT_ADDRESS}/user/changePassword/${id}?token=${token}">alterar sua senha</a>.`, // corpo da mensagem em HTML
     });
   
   }
