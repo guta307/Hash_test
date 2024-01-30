684cbb870d07c051e81652243d907784b5725d22
guta307 <veiga187@gmail.com>
2024-01-30 11:59:08 -0300

    refactor: replace portuguese response by english texts

--- src/decorator/FieldValidation.ts
+++ src/decorator/FieldValidation.ts
@@ -1,44 +1,50 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 
 function FieldValidation<T>(inputObject: T) {
-    return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-      const originalMethod = descriptor.value;
-  
-      descriptor.value = async function(req: Request, res: Response) {
-        
-        try{
-            const inputData = req.body; // Obtenha os dados do corpo da solicitação como T
-  
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = async function (req: Request, res: Response) {
+      try {
+        const inputData = req.body; // Obtenha os dados do corpo da solicitação como T
+
         // Obtenha as chaves do objeto exemplo
-        const requiredFields: (keyof T)[] = Object.keys(inputObject) as (keyof T)[];
-  
+        const requiredFields: (keyof T)[] = Object.keys(
+          inputObject
+        ) as (keyof T)[];
+
         // Verifique se todas as propriedades obrigatórias estão presentes no objeto
         const missingProperties: string[] = [];
-  
+
         for (const key of requiredFields) {
           if (!(key in inputData)) {
             missingProperties.push(key as string);
           }
         }
-  
+
         // Lança um erro se faltarem propriedades
         if (missingProperties.length > 0) {
-          throw new Error(`Faltam propriedades obrigatórias: ${missingProperties.join(', ')}`);
+          throw new Error(
+            `Faltam propriedades obrigatórias: ${missingProperties.join(", ")}`
+          );
         }
-        
-        return originalMethod.apply(this, [req,res]);
-        }
-        catch(e){
-            if (e instanceof Error) {
-                // Se for uma instância de Error, retorne a mensagem de erro
-                return res.status(422).json({ error: e.message });
-              } else {
-                // Para outros tipos de erros, retorne uma mensagem genérica
-                return res.status(500).json({ error: 'Ocorreu um erro desconhecido' });
-              }
+
+        return originalMethod.apply(this, [req, res]);
+      } catch (e) {
+        if (e instanceof Error) {
+          // Se for uma instância de Error, retorne a mensagem de erro
+          return res.status(422).json({ error: e.message });
+        } else {
+          // Para outros tipos de erros, retorne uma mensagem genérica
+          return res.status(500).json({ error: "An unknown error occurred" });
         }
       }
-    }
-  }
+    };
+  };
+}
 
-  export default FieldValidation
+export default FieldValidation;
